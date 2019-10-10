3ffe62b5f13fee05218cf306fc07e09664a6a70b
LittleLostRobotFTC <50188213+LittleLostRobotFTC@users.noreply.github.com>
2019-10-09 19:09:02 -0700

    Added ConfigFactory and RobotConfiguration

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone2019/Config/ConfigFactory.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.Skystone2019.Config;
+
+public class ConfigFactory {
+
+    public static RobotConfiguration Get()
+    {
+        return new RobotConfiguration();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone2019/Config/RobotConfiguration.java
@@ -0,0 +1,16 @@
+/* Little Lost Robots
+   Core Devs: Ryan
+*/
+
+package org.firstinspires.ftc.teamcode.Skystone2019.Config;
+
+public class RobotConfiguration {
+
+    public int FoundationInchesFromWall = 28;
+    public int FoundationInchesMovedTowardsWall = 28;
+    public int FoundationInchesToTheLeftMoved = 30;
+    public int FoundationInchesMoveNextToPlatForm = 9;
+    public int FoundationInchesPlatFormPushed = 8;
+    public int FoundationInchesMovedTowardsBridge = 26;
+    public int FoundationInchesMovedUnderBridge = 18;
+}
